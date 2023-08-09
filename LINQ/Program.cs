ceab30a1581084c6898a0d92b8d4095dff8ace7d
Darlene503 <duyn503@gmail.com>
2023-08-09 08:10:19 -0700

    initial com

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> videoGames = new List<string>
+            {
+                "Mortal Kombat 12",
+                "NBA 2k23",
+                "Halo 2",
+                "Grand Theft Auto 3",
+                "Ninja Gaiden",
+               
+            };
+
+            var orderedGames = videoGames.OrderBy(game => game.Length);
+
+            foreach (var game in orderedGames)
+            {
+                Console.WriteLine(game);
+            }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
